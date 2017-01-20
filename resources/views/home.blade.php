fceb4c748b1e4ba2771ff7787ba9ab325079a12b
bruhla <anthony.bruhl@laposte.net>
2017-01-20 12:52:13 +0000

    Main View Organisation (no aesthetic design) -Add auth stuff -app.blade.php: page structure creation -home.blade.php: content modifications -welcome.blade.php: deleted -web.php: Auth and home page route modifications -HomeController: middleware Auth deleted -config\app.php: app name modification -README.md: content modification

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('title')
+    {{config('app.name')}} - Home
+@endsection
+
+@section('header_title')
+    Home
+@endsection
+
+@section('header_content')
+    Welcome on my Devlog about my work placement experience in IMaR Technology Gateway
+@endsection
+
+
+
+@section('section_title')
+    <div class="container">
+        <div>Company</div>
+    </div>
+@endsection
+
+@section('section_content')
+    IMaR Technology Gateway summary/introduction
+@endsection
+
+@section('article_title')
+    What know about IMaR?
+@endsection
+
+@section('article_content')
+    Blablabla...
+@endsection
+
+
+
+
+
+
