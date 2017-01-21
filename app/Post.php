bc7cbe24287df78f747bd4bd0c88afc9c69524f3
bruhla <anthony.bruhl@laposte.net>
2017-01-21 17:39:20 +0000

    Model creation - Post.php, Role.php, Tag.php, User.php

---
+++ app/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'content', 'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
+}
